03e626ea913cd4757d04c2129779e3a876431395
Risan Ahamed <risanzahamed76@gmail.com>
2022-12-04 12:53:34 +0600

    console removed

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function jwtVerify(req, res, next) {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h3zxwhp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri);
+
 
 async function run() {
 
